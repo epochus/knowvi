d521e83173a6bfbdb95fc9837f24a0d82cb5b1ca
Jackson Wu <Jackson@epochus.local>
2014-05-15 18:07:11 -0500

    fun with forms

--- knowvi/urls.py
+++ knowvi/urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
             name='category'),
 
         url(r'^add_category/$', views.add_category, name='add_category'),
+
+        url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
 )
--- knowvi/views.py
+++ knowvi/views.py
@@ -8,6 +8,7 @@ from knowvi.models import Category, Page
 
 # import Forms
 from knowvi.forms import CategoryForm
+from knowvi.forms import PageForm
 
 def encode_url(str):
     return str.replace(' ', '_')
@@ -46,6 +47,7 @@ def category(request, category_name_url):
     context = RequestContext(request)
     category_name = decode_url(category_name_url)
     context_dict = {'category_name': category_name}
+    context_dict['category_name_url'] = category_name_url
 
     try:
         category = Category.objects.get(name=category_name)
@@ -83,3 +85,47 @@ def add_category(request):
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render_to_response('knowvi/add_category.html', {'form': form}, context)
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+    context_dict = {}
+
+    category_name = decode_url(category_name_url)
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            # This time we cannot commit straight away
+            # Not all fields are automatically populated
+            page = form.save(commit=False)
+
+            # Retrieve the associated Category object so we can add it.
+            # Wrap the code in a try block - check if the category actually exists!
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.category = cat
+            except Category.DoesNotExist:
+                # If we get here, the category does not exist.
+                # Go back and render the add category form as a way of saying the 
+                # the category does not exist.
+                return render_to_response('knowvi/add_category.html', {}, context)
+
+            # Also, create a default value for the number of views
+            page.views = 0
+
+            # With this, we can save our new model instance
+            page.save()
+
+            # Now that the page is saved, display the category instead
+            return category(request, category_name_url)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict['category_name_url'] = category_name_url
+    context_dict['category_name'] = category_name
+    context_dict['form'] = form
+
+    return render_to_response('knowvi/add_page.html',
+                              context_dict, context)
