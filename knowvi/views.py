d8a3bb58cb76de36d22544959584b67eece9c367
Jackson Wu <Jackson@epochus.local>
2014-05-16 18:38:11 -0500

    base templates

--- knowvi/views.py
+++ knowvi/views.py
@@ -239,7 +239,9 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response('knowvi/restricted.html', context_dict, context)
 
 @login_required
 def user_logout(request):
